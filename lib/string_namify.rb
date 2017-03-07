cbc822a893c4c48f07d82cb3d4899ca8b390a40e
Andrea Rampin <andrea.rampin@gmail.com>
2017-03-07 19:18:14 +1100

    Init

---
+++ lib/string_namify.rb
@@ -0,0 +1,13 @@
+String.class_eval do
+  def namify
+    return self if self.empty?
+    capitalized = self.split.map(&:capitalize).join(' ')
+    capitalized.gsub!(/'(?<str>[a-z]{2})/) do |str|
+      "'#{str[1..-1].capitalize}"
+    end
+    capitalized
+  end
+end
+
+module StringNamify
+end
---
+++ spec/string_namify_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe StringNamify do
+  it 'has a version number' do
+    expect(StringNamify::VERSION).not_to be nil
+  end
+
+  it 'namify simple strings' do
+    expect('andrea rampin'.namify).to eq('Andrea Rampin')
+  end
+
+  it 'namify possessive strings' do
+    expect("andrea's cat".namify).to eq("Andrea's Cat")
+  end
+
+  it 'namify strings with apostrophe' do
+    expect("andrea o'rampin".namify).to eq("Andrea O'Rampin")
+  end
+
+  it 'namify strings with dash' do
+    expect("andrea-rampin".namify).to eq("Andrea-rampin")
+  end
+end
